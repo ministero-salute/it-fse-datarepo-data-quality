f0619964e71f537d9af8856f2bdc37658dee7a24
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-08-03 15:59:53 +0200

    fix: refactor test

--- src/test/java/it/finanze/sanita/fse2/dr/dataquality/controllers/base/MockRequests.java
+++ src/test/java/it/finanze/sanita/fse2/dr/dataquality/controllers/base/MockRequests.java
@@ -1,12 +1,15 @@
 package it.finanze.sanita.fse2.dr.dataquality.controllers.base;
 
+import lombok.NoArgsConstructor;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static it.finanze.sanita.fse2.dr.dataquality.utility.RouteUtility.API_REFRESH_SCHEDULER;
 import static it.finanze.sanita.fse2.dr.dataquality.utility.RouteUtility.API_STATUS_HEALTH;
+import static lombok.AccessLevel.PRIVATE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+@NoArgsConstructor(access = PRIVATE)
 public final class MockRequests {
 
     public static MockHttpServletRequestBuilder refreshSchedulerReq() {
